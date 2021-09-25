6849d710d98f9da1edb17e8d9095a23fbee79312
claytonnguyen <claytonnguyen3@gmail.com>
2021-09-25 12:49:21 -0700

    parsing the txt file

--- P1/Program.cs
+++ P1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using static P1.Entree;
 
@@ -8,28 +9,59 @@ namespace P1
     {
         static void Main(string[] args)
         {
-            const string filePath = "";
+            const string filePath = "Users/clayt/Downloads/EntreesTabDelimited.txt";
 
-            //SetUp(filePath);
+            Entree[] nice = SetUp(filePath);
 
-            string name = "Peanut Butter and Jelly";
+            /*string name = "Peanut Butter and Jelly";
             string[] arr = new string[] { "milk", "soy", "lentils", "beans", "veggies" };
             uint[] arr1 = new uint[] { 0, 12, 12, 1, 0, 0, 0, 2, 5, 3, 67, 3 };
             Entree food = new Entree(name, arr, arr1);
-            Console.WriteLine(food.Contains("milk"));
+            Console.WriteLine(food.Contains("milk"));*/
         }
 
         static public Entree[] SetUp(string filePath)
         {
             Entree[] Arr = new Entree[] { };
             bool reading = true;
+            char[] delimiter = new char[] { '\t' };
+
+            try
+            {
+                StreamReader sr = new StreamReader("C:\\" + filePath);
+
+                string[] line = sr.ReadLine().Split(delimiter);
+
+                while (line != null)
+                {
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        Console.WriteLine(line[i]);
+                    }
+                    line = sr.ReadLine().Split(delimiter);
+                }
+
+                sr.Close();
+                Console.ReadLine();
+
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
 
             while (reading)
             {
+
                 string name = "";
                 string[] ingredients = new string[] { };
                 uint[] nutritionStats = new uint[12];
-                // read in data from text file
+                
 
                 Entree food = new Entree(name, ingredients, nutritionStats);
 
